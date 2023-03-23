4dd653144160fb13550fd0dad64ee35c79fd6596
Ryan Meza <ryanmeza116@gmail.com>
2023-03-23 14:51:16 -0400

    first

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    NavBar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    FeedbackCard,
+    GetStarted
+
+
+};
